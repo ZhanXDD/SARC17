8b99e26565f951dbf2c6ef3d830edf427b435d8e
ZhanXDD <elgor2k1@gmail.com>
2021-11-16 14:21:46 +0100

    menu style and file created

---
+++ php/menu.php
@@ -0,0 +1,10 @@
+<div id='page-wrap'>
+<link rel="stylesheet" href="../style/menu.css">
+<header class='main' id='h1'>
+    <span class="left">a</span>
+    <span class="right">
+        <a href="../php/register.php">Registro</a>
+        <a href="../php/login.php">Login</a>
+    </span>
+</header>
+</div>
