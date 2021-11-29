431b10ecabb003262cc6e696108d43c2add38c1c
ZhanXDD <elgor2k1@gmail.com>
2021-11-29 11:52:55 +0100

    menu

--- php/menu.php
+++ php/menu.php
@@ -1,9 +1,9 @@
 <div id='page-wrap'>
 <link rel="stylesheet" href="../style/menu.css">
 <header class='main' id='h1'>
-    <span class="left">a</span>
+    <span class="left"></span>
     <span class="right">
-        <a href="../php/register.php">Registro</a>
+        <a href="../php/register.php">Registrarse</a>
         <a href="../php/login.php">Login</a>
     </span>
 </header>
