80b66eb0dc692dc85e540258232089bb43aed4ac
BernalDavid <davidbernal2001@gmail.com>
2021-12-04 22:50:14 +0100

    viewProductList fixed, added css

--- php/viewProductList.php
+++ php/viewProductList.php
@@ -16,41 +16,45 @@
         <?php
             try {
                 //Open connection with the databse
-                $dsn ="mysql:dbname=$basededatos;host=$local";
+                $dsn ="mysql:dbname=$basededatos;host=$server";
                 $dbh = new PDO($dsn, $user, $pass);
 
                 //Prepare statement
                 $stmt = $dbh -> prepare("SELECT * FROM product");
 
                 //Execute statement
-                $stmt = $dbh -> execute();
+                $stmt -> execute();
 
-                //Get the number of diferent products (number of rows)
-                $numProducts=$stmt->rowCount();
-
-                //No products available
-                if($numProducts==0) {
-                    echo('No hay ningún producto en nuestra tienda');
-                }
-                //Show all the available products 
-                else {
-                    foreach($stmt->fetchAll(PDO::FETCH_OBJ) as $producto) {
-                        echo('<div class="InfoProducto>');
-                        echo('Nombre de producto:   '.$producto->name.'<br>');
-                        echo('Tipo de producto:     '.$producto->type.'<br>');
-                        echo('Precio:               '.$producto->price.' € <br>');
-                        echo('Unidades disponibles: '.$producto->stock.'<br>');
-                        if($producto->descripcion) {
-                            echo('Descripción:          '.$producto->description.'<br>');
-                        }
-                        echo('</div>');
-                        echo('<br>');
-                    }
-                }
+                //close connection
+                $dbh = null;
             }
             catch(PDOException $e) {
                 echo $e -> getMessage();
-            }  
+            }
+
+            //Get the number of diferent products (number of rows)
+            $numProducts=$stmt->rowCount();
+
+            //No products available
+            if($numProducts==0) {
+                echo('No hay ningún producto en nuestra tienda');
+            }
+            
+            //Show all the available products 
+            else {
+                foreach($stmt->fetchAll(PDO::FETCH_OBJ) as $producto) {
+                    echo('<div class="InfoProducto">');
+                    echo('Nombre de producto:   '.$producto->name.'<br>');
+                    echo('Tipo de producto:     '.$producto->type.'<br>');
+                    echo('Precio:               '.$producto->price.' € <br>');
+                    echo('Unidades disponibles: '.$producto->stock.'<br>');
+                    if($producto->descripcion) {
+                        echo('Descripción:          '.$producto->description.'<br>');
+                    }
+                    echo('</div>');
+                    echo('<br>');
+                }
+            } 
         ?>  
 	</body>
 </html>
