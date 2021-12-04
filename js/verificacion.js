1895bb04bf1294a52bcc05288a4a18cf3b2cf694
ZhanXDD <elgor2k1@gmail.com>
2021-12-04 19:01:40 +0100

    comments added

--- js/verificacion.js
+++ js/verificacion.js
@@ -1,108 +1,92 @@
-/**
- * Function to verify if inserted parameter is empty
- * @param {*} input to check for empty elements
- * @returns true if there is an empty element, false otherwise
- */
-function emptyValues(input){
-    if(input == ""){
-        console.log("true");
-        return true;
-    }
-    console.log("false");
-    return false;
-}
-
 //When document ready
 $(function(){
 
-    //When element with id name has lost focus
+    //Event when name has lost focus
     $('#name').on('blur', function(){
-        //check if name is empty
         if(!this.value)
             $('#nameError').text('El campo esta vacio');
     });
-
+    //Event when name has changed input text
     $('#name').on('input', function(){
-        //check if name is not empty
         if(this.value)
             $('#nameError').text('');
     });
 
-    //When element with id email has lost focus
+    //Event when email has lost focus
     $('#email').on('blur', function(){
-        //check if email does not comply with regexp
         if(!this.value.match(/^.+@.*\..{2,}$/))
             $('#emailError').append('El correo tiene el formato incorrecto');
     });
-
+    //Event when email has changed input text
     $('#email').on('input', function(){
-        //check if email complies with regexp
         if(this.value.match(/^.+@.*\..{2,}$/))
             $('#emailError').text('');
     });
 
-    //When element with id phone has lost focus
+    //Event when phone has lost focus
     $('#phone').on('blur', function(){
         //if phone has value, check if it contains at least 9 digits
         if(this.value && !this.value.match(/^[0-9]{9,}$/)){
             $('#phoneError').text('El telefono tiene tener al menos nueve numeros');
         }
     });
-
+    //Event when phone has change input text
     $('#phone').on('input', function(){
-        //if phone has value, check if it contains at least 9 digits
+        //if phone doesn't has value or check if it contains at least 9 digits
         if(!this.value || this.value.match(/^[0-9]{9,}$/)){
             $('#phoneError').text('');
         }
     });
 
-    //When element with id password has lost focus
+    //Event when password has lost focus
     $('#password').on('blur', function(){
         //check if password has 8 characters
         if(!this.value.match(/^.{8,}$/))
             $('#passError').text('La contraseña tiene que tener al menos ocho caracteres');
     });
-
+    //Event when email has changed input text
     $('#password').on('input', function(){
         //check if password has 8 characters
         if(this.value.match(/^.{8,}$/))
             $('#passError').text('');
     });
 
-    //When element with id password2 has lost focus
+    //Event when password2 has lost focus
     $('#password2').on('blur', function(){
         //check for different password
         if(this.value != $('#password').val())
             $('#pass2Error').text('La contraseña es diferente');
     });
-
+    //Event when password2 has changed input text
     $('#password2').on('input', function(){
         //check for different password
         if(this.value == $('#password').val())
             $('#pass2Error').text('');
     });
 
+    //Event when form is submmited
     $('#form').on('submit', function(event){
         
         let errors = [$('#nameError').text(), $('#emailError').text(), $('#phoneError').text(), $('#passError').text(), $('#pass2Error').text()];
         let inputs = [$('#name').val(), $('#email').val(), $('#password').val(), $('#password2').val()];
+        
+        //check for empty required inputs
         for(let i = 0; i < inputs.length; i++){
-            console.log(inputs[i]);
             if(!inputs[i]){
                 event.preventDefault();
                 $('#generalError').text('Hay campos obligatorios no llenos');
-                console.log('datos vacios');
                 return false;
             }
         }
+
+        //check for shown errors
         for(let i = 0; i < errors.length; i++){
-            console.log(errors[i]);
             if(errors[i]){
                 event.preventDefault();
-                console.log('datos incorrecto');
                 $('#generalError').text('Hay datos introducidos erroneos');
                 return false;
             }
         }
+        return true;
     });
 });
