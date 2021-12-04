0eec847569045d0dc5387d02f829f93060a48c38
ZhanXDD <elgor2k1@gmail.com>
2021-12-04 18:16:15 +0100

    verify register

--- js/verificacion.js
+++ js/verificacion.js
@@ -1,52 +1,107 @@
-$(function(){
-    function verificarRegistro(){
-        let nombre = $("#nombre");
-        let correo = $("#correo");
-        let numero = $("#numero");
-        let pass = $("#password");
-        let pass2 = $("#password2");
-
-        const fill = [nombre, correo, pass, pass2];
-        if(emptyValues(fill)){
-            //escribir error
-            return false;
-        }
-
-        if(incorrectFormat(correo)){
-            //escribir error
-            return false;
-        }
-
-        if(!pass.match(/^.{8,}$/)){
-
-        }
-    }
-
-    /**
-     * Function to verify if array has empty elements
-     * @param {*} array to check for empty elements
-     * @returns true if there is an empty element, false otherwise
-     */
-    function emptyValues(array){
-        array.forEach(function(value){
-            if(!value.value){
-                console.log("ERROR VALORES VACIOS");
-                return true;
-            }
-        });
-        return false;
-    }
-
-    /**
-     * Function to verify if email is in a valid format
-     * @param {*} correo the email to verify
-     * @returns true if the email is in an invalid format, false otherwise
-     */
-    function incorrectFormat(correo){
-        if(correo.match(/^.+@.*\..{2,}$/)){
-            console.log("ERROR CORREO FORMATO INCORRECTO");
-            return false;
-        }
+/**
+ * Function to verify if inserted parameter is empty
+ * @param {*} input to check for empty elements
+ * @returns true if there is an empty element, false otherwise
+ */
+function emptyValues(input){
+    if(input == ""){
+        console.log("true");
         return true;
     }
+    console.log("false");
+    return false;
+}
+
+//When document ready
+$(function(){
+
+    //When element with id name has lost focus
+    $('#name').on('blur', function(){
+        //check if name is empty
+        if(!this.value)
+            $('#nameError').text('El campo esta vacio');
+    });
+    
+    $('#name').on('input', function(){
+        //check if name is not empty
+        if(this.value)
+            $('#nameError').text('');
+    });
+
+    //When element with id email has lost focus
+    $('#email').on('blur', function(){
+        //check if email does not comply with regexp
+        if(!this.value.match(/^.+@.*\..{2,}$/))
+            $('#emailError').text('El correo tiene el formato incorrecto');
+    });
+
+    $('#email').on('input', function(){
+        //check if email complies with regexp
+        if(this.value.match(/^.+@.*\..{2,}$/))
+            $('#emailError').text('');
+    });
+
+    //When element with id phone has lost focus
+    $('#phone').on('blur', function(){
+        //if phone has value, check if it contains at least 9 digits
+        if(this.value && !this.value.match(/^[0-9]{9,}$/)){
+            $('#phoneError').text('El telefono tiene tener al menos nueve numeros');
+        }
+    });
+
+    $('#phone').on('input', function(){
+        //if phone has value, check if it contains at least 9 digits
+        if(!this.value || this.value.match(/^[0-9]{9,}$/)){
+            $('#phoneError').text('');
+        }
+    });
+
+    //When element with id password has lost focus
+    $('#password').on('blur', function(){
+        //check if password has 8 characters
+        if(!this.value.match(/^.{8,}$/))
+            $('#passError').text('La contraseña tiene que tener al menos ocho caracteres');
+    });
+
+    $('#password').on('input', function(){
+        //check if password has 8 characters
+        if(this.value.match(/^.{8,}$/))
+            $('#passError').text('');
+    });
+
+    //When element with id password2 has lost focus
+    $('#password2').on('blur', function(){
+        //check for different password
+        if(this.value != $('#password').val())
+            $('#pass2Error').text('La contraseña es diferente');
+    });
+
+    $('#password2').on('input', function(){
+        //check for different password
+        if(this.value == $('#password').val())
+            $('#pass2Error').text('');
+    });
+
+    $('#form').on('submit', function(event){
+        
+        let errors = [$('#nameError').val(), $('#emailError').val(), $('#phoneError').val(), $('#passError').val(), $('#pass2Error').val()];
+        let inputs = [$('#name').val(), $('#email').val(), $('#password').val(), $('#password2').val()];
+        
+        for(let i = 0; i < inputs.length; i++){
+            if(!inputs[i]){
+                event.preventDefault();
+                $('#generalError').text('Hay campos obligatorios no llenos');
+                console.log('datos vacios');
+                return false;
+            }
+        }
+        for(let i = 0; i < errors.length; i++){
+            if(!errors[i]){
+                event.preventDefault();
+                console.log('datos incorrecto');
+                $('#generalError').text('Hay datos introducidos erroneos');
+                return false;
+            }
+        }
+    });
 });
