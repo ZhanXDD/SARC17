062d88968255537e4455d2f273f5b5afe1230883
ZhanXDD <elgor2k1@gmail.com>
2021-12-05 16:52:58 +0100

    php error feedback verification

--- php/register.php
+++ php/register.php
@@ -3,39 +3,49 @@
 <?php
 	//a form was sent
 	$feedback = "";
+	$errorName = "";
+	$errorPass = "";
+	$errorPass2 = "";
+	$errorPhone = "";
+	$errorEmail = "";
 	if(isset($_POST['submit'])){
 		//check for empty name
 		if($_POST['name'] == ""){
+			$errorName = "El nombre esta vacio";
 			$feedback = "error";
 		}
 
 		//check for password with at least 8 character
 		if(!preg_match("/^.{8,}$/",$_POST['password'])){
+			$errorPass = "La contraseña tiene que tener al menos ocho caracteres";
 			$feedback = "error";
 		}else{
 			//Check if password and password2 are not the same
 			if(!($_POST['password'] == $_POST['password2'])){
+				$errorPass2 = "Las contraseñas no coinciden";
 				$feedback = "error";
 			}
 		}
 
-		//Check if numer is empty
+		//Check if phone is empty
 		if($_POST['phone'] !== ""){
 			if(!preg_match("/^\d{9,}$/",$_POST['phone'])){
+				$errorPhone = "El numero de telefono tiene que tener al menos 9 numeros";
 				$feedback = "error";
 			}
 		}
 
 		$regex = "/^.+@.*\..{2,}$/";
-		//check for correct format
+		//check for correct format for email
 		if(!preg_match($regex,$_POST['email'])){
+			$errorEmail = "El email tiene el formato el incorrecto";
 			$feedback = "error";	
 		}
 
 		//There has been no error
 		if($feedback === ''){
 			try{
-				$dsn = "mysql:dbname=$basededatos;host=$local";
+				$dsn = "mysql:dbname=$basededatos;host=$server";
 				$dbh = new PDO($dsn, $user, $pass);
 				//prepared statement
 				$stmt = $dbh -> prepare("INSERT INTO user VALUES (?,?,?,?)");
@@ -50,21 +60,21 @@
 			}
 			$dbh = null;
 
-			//move to login
-			$root = simplexml_load_file("../xml/activeUsers.xml");
-			$user = $root -> addchild("user");
-			$user -> addAttribute("name",$_POST['name']);
-			$user -> addChild("email",$_POST['email']);
+			// //move to login
+			// $root = simplexml_load_file("../xml/activeUsers.xml");
+			// $user = $root -> addchild("user");
+			// $user -> addAttribute("name",$_POST['name']);
+			// $user -> addChild("email",$_POST['email']);
 			
-			//Formating XML
-			$dom = new DOMDocument("1.0");
-			$dom->preserveWhiteSpace = false;
-			$dom->formatOutput = true;
-			$dom->loadXML($root->asXML());
+			// //Formating XML
+			// $dom = new DOMDocument("1.0");
+			// $dom->preserveWhiteSpace = false;
+			// $dom->formatOutput = true;
+			// $dom->loadXML($root->asXML());
 
-			//Save xml
-			$xml = new SimpleXMLElement($dom->saveXML());
-			$xml -> asXML("../xml/users.xml");
+			// //Save xml
+			// $xml = new SimpleXMLElement($dom->saveXML());
+			// $xml -> asXML("../xml/users.xml");
 		}
 	}
 ?>
@@ -74,7 +84,7 @@
 		<title>Registrarse</title>
 		<meta charset="UTF-8">
 		<script src='../js/jquery-3.4.1.min.js'></script>
-		<script src="../js/verificacion.js"></script>
+	<!--	<script src="../js/verificacion.js"></script> -->
 		<link rel="stylesheet" href="../style/errors.css">
 	</head>
 	<body>
@@ -84,22 +94,22 @@
 			<form method='POST' id='form'>
 				Nombre de usuario<small>*</small>: 
 				<input type='text' id='name' name="name">
-				<span class="error" id="nameError"></span><br>
+				<span class="error" id="nameError"><?php echo $errorName;?></span><br>
 				
-				Correo electronco<small>*</small>: 
+				Correo electronico<small>*</small>: 
 				<input type='text' id='email' name="email">
-				<span class="error" id="emailError"></span><br>
+				<span class="error" id="emailError"><?php echo $errorEmail;?></span><br>
 				
 				Numero de telefono: 
 				<input type='text' id='phone' name="phone">
-				<span class="error" id="phoneError"></span><br>
+				<span class="error" id="phoneError"><?php echo $errorPhone;?></span><br>
 				
 				Contraseña<small>*</small>: 
 				<input type='password' id='password' name="password">
-				<span class="error" id="passError"></span><br>
+				<span class="error" id="passError"><?php echo $errorPass;?></span><br>
 				Repetir contraseña<small>*</small>: 
 				<input type='password' id='password2' name="password2">
-				<span class="error" id="pass2Error"></span><br>
+				<span class="error" id="pass2Error"><?php echo $errorPass2;?></span><br>
 				
 				<input type='submit' id='submit' name="submit" value='Registrarse'><br>
 			</form>
