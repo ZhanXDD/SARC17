0cf6a886daa0dadf366638b642a89eeb0149298c
BernalDavid <davidbernal2001@gmail.com>
2021-12-06 19:46:54 +0100

    Update viewProductList.php

--- php/viewProductList.php
+++ php/viewProductList.php
@@ -54,7 +54,7 @@
                         echo('Descripción:          '.'<br>');
                         echo(''.$producto->description.'<br>');
                     }*/
-                    echo('<img src="../img/'.$producto->id.'.jpg" alt="No hay imagen"><br><br>');
+                    echo('<img src="../img/'.$producto->id.'.jpg" class="right" alt="No hay imagen"><br><br>');
                     echo('<input type="submit" value="comprar objeto" onclick="buyProduct('.$producto->id.')"> </input>');
                     echo('</div>');
                     echo('<br>');
