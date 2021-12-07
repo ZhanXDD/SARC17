0151a6b3d69995155117fa97adf556715224cfec
ZhanXDD <elgor2k1@gmail.com>
2021-12-07 18:12:39 +0100

    fixed menu

--- php/menu.php
+++ php/menu.php
@@ -2,6 +2,7 @@
 <div id='page-wrap'>
 <link rel="stylesheet" href="../style/menu.css">
 <header class='main' id='h1'>
+    <?php print_r($_SESSION)?>
     <span class="left">
         <a href="../php/viewProductList.php">Produtos</a>
     </span>
@@ -9,12 +10,12 @@
     <?php
         if(isset($_SESSION['name'])){
     ?>
-        <a href="../php/register.php">Registrarse</a>
-        <a href="../php/logIn.php">Iniciar Sesion</a>
+        <a href="../php/logOut.php">Cerrar Sesion</a>
     <?php    
         }else{
     ?>
-        <a href="../php/logOut.php">Cerrar Sesion</a>
+        <a href="../php/register.php">Registrarse</a>
+        <a href="../php/logIn.php">Iniciar Sesion</a>
     <?php
         }
     ?>
