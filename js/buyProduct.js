3a29d67d4caf75d1ed96c72d8a8b03d38d1c646c
Yencr0s <roberria5@gmail.com>
2021-12-07 21:27:26 +0100

    parte 2

--- js/buyProduct.js
+++ js/buyProduct.js
@@ -14,3 +14,51 @@ function goProfile(email) {
     window.location.href="viewProductList.php?email="+email;
 }
 
+function buyProduct(productId) {
+    window.location.href="buy.php?id="+productId;
+}
+
+function goPurchaseEnd(productId) {
+    window.location.href="purchaseEnd.php?id="+productId;
+}
+
+function goProductList() {
+    window.location.href="viewProductList.php";
+}
+
+$(function(){
+    $('#credit_card').on('input', function(){
+        if(this.value)
+            $('#credit_cardError').text('');
+    });
+
+    //ANCHOR no se como poner el card_type en el php
+    $('#card_number').on('input', function(){
+        if(this.value.match(/^4[0-9]{12}(?:[0-9]{3})?$/)){
+            $('#card_type').text('Visa')
+            $('#card_numberError').text('');
+        }else if(this.value.match(/(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$ /)){
+            $('#card_type').text('Mastercard')
+            $('#card_numberError').text('');
+        }else if(this.value===''){
+            $('#card_type').text('')
+            $('#card_numberError').text('Introduzca la tarjeta de credito');
+        }else{
+            $('#card_type').text('')
+            $('#card_numberError').text('Solo aceptamos Visa o Mastercard');
+        }
+    });
+
+    $('#expiration_date').on('input', function(){
+        if(this.value)
+            $('#expiration_dateError').text('');
+    });
+
+    $('#card_cvc').on('input', function(){
+        if(this.value.match(/^[0-9]{3}*$/))
+            $('#card_cvcError').text('');
+    });
+
+    
+});
+
--- php/purchaseEnd.php
+++ php/purchaseEnd.php
@@ -39,34 +39,90 @@
                 echo('<div class="form"> En estos momentos no disponemos del producto seleccionado, sentimos las molestias');
             }
             else {
-                $productStock=$productStock-1;
-                //Open connection with the databse
-                $dsn ="mysql:dbname=$basededatos;host=$server";
-                $dbh = new PDO($dsn, $user, $pass);
-                //Prepare the statement
-                $stmt = $dbh -> prepare('UPDATE product SET stock='.$productStock.' WHERE id=?');
-                $stmt -> bindParam(1, $_GET['id']);
-                //execute statement
-                $stmt -> execute();
+                $feedbackCredit_card = "";
+                $feedbackCard_number = "";
+                $feedbackExpiration_date = "";
+                $feedbackCard_cvc= "";
+                
+                if(isset($_POST['submit'])){
+                    //check for empty name
+                    if($_POST['credit_card'] == ""){
+                        $feedbackName = "Falta el nombre del titular";
+                        $feedback = "error";
+                    }
 
-                //Prepare the statement
-                $stmt = $dbh -> prepare('UPDATE user SET articles=articles+'.1.' WHERE email=?');
-                $stmt -> bindParam(1, $_SESSION['email']);
-                //execute statement
-                $stmt -> execute();
+                    //check for empty card number
+                    if($_POST['card_number'] == ""){
+                        $feedbackName = "Falta el número de la tarjeta de crédito";
+                        $feedback = "error";
+                    }
 
-                //Prepare the statement
-                $stmt = $dbh -> prepare('UPDATE user SET spent=spent+'.$productPrice.' WHERE email=?');
-                $stmt -> bindParam(1, $_SESSION['email']);
-                //execute statement
-                $stmt -> execute();
+                    //check for empty expiration date
+                    if($_POST['expiration_date'] == ""){
+                        $feedbackName = "Falta la fecha de caducidad";
+                        $feedback = "error";
+                    }
 
-                echo("<h1> Gracias por su compra </h1>");
-                echo('<div class="form">'.$productName.' adquirido correctamente.');
-            }
+                    //check for empty cvc
+                    if($_POST['card_cvc'] == ""){
+                        $feedbackName = "Falta el cvc";
+                        $feedback = "error";
+                    }
+                    if($feedback===''){
+  
+                        $productStock=$productStock-1;
+                        //Open connection with the databse
+                        $dsn ="mysql:dbname=$basededatos;host=$server";
+                        $dbh = new PDO($dsn, $user, $pass);
+                        //Prepare the statement
+                        $stmt = $dbh -> prepare('UPDATE product SET stock='.$productStock.' WHERE id=?');
+                        $stmt -> bindParam(1, $_GET['id']);
+                        //execute statement
+                        $stmt -> execute();
+
+                        //Prepare the statement
+                        $stmt = $dbh -> prepare('UPDATE user SET articles=articles+'.1.' WHERE email=?');
+                        $stmt -> bindParam(1, $_SESSION['email']);
+                        //execute statement
+                        $stmt -> execute();
+
+                        //Prepare the statement
+                        $stmt = $dbh -> prepare('UPDATE user SET spent=spent+'.$productPrice.' WHERE email=?');
+                        $stmt -> bindParam(1, $_SESSION['email']);
+                        //execute statement
+                        $stmt -> execute();
+                        
+                        echo("<h2>div Gracias por su compra </h2>");
+                        echo('<div class="form">'.$productName.' adquirido correctamente.');
+                    }
+                    echo('<h1>Introduzca el método de pago</h1><br>
+                        <div class = "form" id = "form">
+                            <form method='POST' id='form'>
+                                Nombre del titular: 
+                                <input type='text' id='credit_card' name="credit_card"><br>
+                                <span class="error"><?php echo $feedbackCredit_card;?></span><br>
+
+                                Numero de tarjeta: 
+                                <input type='text' id='card_number' name="card_number"><br>
+                                <span class="error"><?php echo $feedbackCard_number;?></span><br>
+
+                                Fecha de caducidad: 
+                                <input type='date' id='expiration_date' name="expiration_date"><br>
+                                <span class="error"><?php echo $feedbackExpiration_date;?></span><br>
+
+                                CVC: 
+                                <input type='text' id='card_cvc' name="card_cvc"><br>
+                                <span class="error"><?php echo $feedbackCard_cvc;?></span><br>
+
+                                <input type='submit' id='submit' name="submit" value='Comprar'><br>
+                            </form>
+                            <span class='error'><?php echo $feedback;?></span>
+                        </div>');
+                }
             echo('<input type="submit" value="volver a la tienda" onclick="goProductList()"> </input>');
             echo('<input type="submit" value="cerrar sesión" onclick="logOut()"> </input>');
             echo('</div>');
+            }
         ?>
          
     </body>
