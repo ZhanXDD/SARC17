0ac0765815c7c3acd4276dd72077a722efe82513
BernalDavid <davidbernal2001@gmail.com>
2021-12-07 18:57:38 +0100

    purchaseEnd added

--- js/buyProduct.js
+++ js/buyProduct.js
@@ -1,4 +1,12 @@
-
 function buyProduct(productId) {
     window.location.href="buy.php?id="+productId;
 }
+
+function goPurchaseEnd(productId) {
+    window.location.href="purchaseEnd.php?id="+productId;
+}
+
+function goProductList() {
+    window.location.href="viewProductList.php";
+}
+
